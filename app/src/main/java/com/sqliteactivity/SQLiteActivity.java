4a007406fca39777390b214d5f21b0fe94776d97
Softwarica College Lab <softwarica@softwarica.com>
2019-05-02 12:05:35 +0545

    Initial commit

---
+++ app/src/main/java/com/sqliteactivity/SQLiteActivity.java
@@ -0,0 +1,22 @@
+package com.sqliteactivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import helper.MyHelper;
+
+public class SQLiteActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sqlite);
+
+    }
+}
